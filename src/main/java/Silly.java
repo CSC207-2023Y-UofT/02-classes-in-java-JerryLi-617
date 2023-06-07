fae75574f0ab5311fe7f5863bd9d0b28db94544a
Jerry Li <jerryw.li@mail.utoronto.ca>
2023-06-07 16:59:24 -0400

    Finished and passed all test.

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -79,6 +79,16 @@ public class Silly implements Comparable<Silly>{
      *       strings (e.g. this.name = [first string] + [second string]).
      *       Make sure you document this method!
      */
+    /**
+     * Creates a new Silly object.
+     * This constructor takes in two String as arguments.
+     *
+     * @param str1 a string for changing this Silly instance's name.
+     * @param str2 a second string for changing this Silly instance's name.
+     */
+    public  Silly(String str1, String str2){
+        this.name = str1 + str2;
+    }
 
 
 
@@ -116,7 +126,7 @@ public class Silly implements Comparable<Silly>{
         y.countStatic();
         x.countStatic();
         x.countStatic();
-        int[] expected_values = {};
+        int[] expected_values = {0, 1, 2, 3};
 
         System.out.println("The countStatic calls will return " + Arrays.toString(expected_values));
     }
@@ -134,6 +144,7 @@ public class Silly implements Comparable<Silly>{
     @Override
     public String toString(){
         // TODO (Task 3): Implement the body of this method!
+        return this.name;
     }
 
     /**
@@ -158,6 +169,11 @@ public class Silly implements Comparable<Silly>{
         }
 
         Silly other = (Silly) o; // To access .name of o, we need to cast it.
+        if (this.name.equals(other.name)){
+            return true;
+        } else{
+            return false;
+        }
 
         // Hint: to compare strings, we need to use .equals()
         //       e.g. s1.equals(s2)
@@ -194,6 +210,10 @@ public class Silly implements Comparable<Silly>{
          *                You can get the length of a string by using the
          *                .length() method.
          */
+        int otherLength = other.name.length();
+        int thisLength = this.name.length();
+
+        return thisLength - otherLength;
     }
 
     /*
